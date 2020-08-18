18db4b6e7c73eaffee0e5e2635c29c44dc059cd9
Younes <alturkeyy@gmail.com>
2020-08-18 07:59:24 +0300

    deploy build

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitRegister = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://boiling-beach-51545.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
--- src/containers/App.js
+++ src/containers/App.js
@@ -85,7 +85,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://boiling-beach-51545.herokuapp.com/imageurl',{
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -95,7 +95,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:3000/image',{
+        fetch('https://boiling-beach-51545.herokuapp.com/image',{
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
