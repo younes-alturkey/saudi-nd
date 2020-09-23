10718c49ffa67a0a2c5d4deb0a1618597b1e05af
Younes <alturkeyy@gmail.com>
2020-09-23 09:59:10 +0300

    Updates

---
+++ precache-manifest.a040f7dadd2ae6ed5960747ef216f7b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6603a252db71daed94abcada9f5d983f",
+    "url": "/saudi-nd/index.html"
+  },
+  {
+    "revision": "6479433a0366c6d61287",
+    "url": "/saudi-nd/static/css/2.058ca934.chunk.css"
+  },
+  {
+    "revision": "63b17916f7fb1b536c8a",
+    "url": "/saudi-nd/static/css/main.b8c32353.chunk.css"
+  },
+  {
+    "revision": "6479433a0366c6d61287",
+    "url": "/saudi-nd/static/js/2.722fcdb4.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/saudi-nd/static/js/2.722fcdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b17916f7fb1b536c8a",
+    "url": "/saudi-nd/static/js/main.a4f1a77d.chunk.js"
+  },
+  {
+    "revision": "1fa3f75e1372b4c12484",
+    "url": "/saudi-nd/static/js/runtime-main.aa768989.js"
+  },
+  {
+    "revision": "1c9d6e86999e295fb35982c70cbb26c5",
+    "url": "/saudi-nd/static/media/iris.1c9d6e86.png"
+  },
+  {
+    "revision": "3185ee1bca04431dbb4b41496d0b9c71",
+    "url": "/saudi-nd/static/media/kings.3185ee1b.png"
+  }
+]);
